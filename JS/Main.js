b4673b6c8fa8cbe7a4c2c7575c2d4bf63d1dfc54
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-03-23 18:17:52 +0100

    Update 1.2 - added new codes and typed animation

--- JS/Main.js
+++ JS/Main.js
@@ -1,1 +1,10 @@
-
+function Onload() 
+{
+    var typed = new Typed('#typing', {
+        strings: ["Your Self", "Your Life", "Your Family", "Your Friends", "Your Time"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 2000,
+        backSpeed: 30
+    }); 
+}
